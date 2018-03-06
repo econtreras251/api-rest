ecf6d8b3354fdcdbffbd96c52964c49df722eeb6
Edgar Contreras <ec542498@gmail.com>
2018-03-06 09:26:13 +0100

    Se paso a mejorar los metodos HTTP
    Se proporciono la completacion de los modelos y su valores devueltos
    segun sus peticiones, ademas de enlazar esos registros con la base de
    datos

--- index.js
+++ index.js
@@ -13,11 +13,24 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.get('/api/product',(req, res)=>{
-  res.status(200).send( { products: []} )
+  Product.find({}, (err, products)=>{//Busqueda de todo
+    if(err) res.status(500).send({ message: `Error al realizar la peticion ${err}`})
+    if(!products) return res.status(404).send({message: `No existen Productos`})
+
+    res.status(200).send( { products })
+  })
+
 })
 
 app.get('/api/product/:productId', (req, res)=>{
+  let productId = req.params.productId
 
+  Product.findById(productId, (err, product)=>{
+    if(err) return res.status(500).send({message: `Erro al realizar la busqueda ${err}`})
+    if(!product) return res.status(404).send({message: `Error el producto no existe`})
+
+    res.status(200).send({ product })
+  })
 })
 
 app.post('/api/product', (req, res)=>{
