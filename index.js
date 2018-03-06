9afc1775c3eba47f21fb9b7f5ace935aa3b22edd
Edgar Contreras <ec542498@gmail.com>
2018-03-06 19:24:58 -0430

    Se crea el archivo config

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose')
 const app = require('./app')
-const port = process.env.PORT || 3001
+const config = require('./config.js')
 
-mongoose.connect('mongodb://localhost:27017/shop', (err,res)=>{
+mongoose.connect(config.db, (err,res)=>{
   if(err) {
     return console.log(`Error al conectar a la base de datos: ${err}`)
   }
   console.log('Conexion a la base de datos establecida...')
 
-  app.listen(port, ()=>{
-    console.log(`API REST corriendo en http://localhost:${port}`)
+  app.listen(config.port, ()=>{
+    console.log(`API REST corriendo en http://localhost:${config.port}`)
   })
 })
