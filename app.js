c34cf8d037d9880aef634b6d11903792024236d5
Edgar Contreras <ec542498@gmail.com>
2018-03-06 19:13:58 -0430

    Se dividio las rutas y se incluyo el nav

--- app.js
+++ app.js
@@ -3,15 +3,10 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const productCtrl = require('./controllers/product')
+const api = require('./routes')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-
-app.get('/api/product', productCtrl.getProducts)
-app.get('/api/product/:productId', productCtrl.getProduct)
-app.post('/api/product', productCtrl.saveProduct)
-app.put('/api/product/:productId', productCtrl.updateProduct)
-app.delete('/api/product/:productId', productCtrl.deleteProduct)
+app.use('/api', api)
 
 module.exports = app
